3f1197aedb954c6d8237e7316aff16a1fa1c5504
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-11-25 01:21:32 +1100

    implement CHIP-8 cpu in rust

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-use crate::bits::floating_point_partial_eq;
+use cpu::CPU;
 
 pub mod bits;
-mod cpu;
+pub mod cpu;
 
 fn main() {
-    floating_point_partial_eq();
-}
+    let mut cpu = CPU::new();
 
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+
+    let mem = &mut cpu.memory;
+    mem[0x000] = 0x21; mem[0x001] = 0x00;
+    mem[0x002] = 0x21; mem[0x003] = 0x00;
+    mem[0x004] = 0x00; mem[0x005] = 0x00;
+
+    mem[0x100] = 0x80; mem[0x101] = 0x14;
+    mem[0x102] = 0x80; mem[0x103] = 0x14;
+    mem[0x104] = 0x00; mem[0x105] = 0xEE;
+
+    cpu.run();
+
+    assert_eq!(cpu.registers[0], 45);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
+}
